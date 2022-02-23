d0cbd38b05beec7bb6ae05f4af4b66c8e719de7a
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-02-23 17:16:10 +0100

    force_https

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+4
 /*
 |--------------------------------------------------------------------------
 | Web Routes
