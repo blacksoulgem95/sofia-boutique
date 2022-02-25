0ae5acef3071026226f6631a8b84c7c409da15be
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-02-25 09:16:50 +0100

    fix style

--- routes/admin.php
+++ routes/admin.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->middleware('role:ADMIN')->group(function () {
-    Route::get('/admin', fn() => view('admin.dashboard'))->name('admin.dashboard');
+    Route::get('/admin', fn () => view('admin.dashboard'))->name('admin.dashboard');
 });
